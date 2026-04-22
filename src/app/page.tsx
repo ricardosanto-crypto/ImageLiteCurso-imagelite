077cb9f8ccad2f13c9280ef7afc4f781864f5cd0
ricardosanto-crypto <ricardo.santo@gmail.com>
2026-04-22 11:13:48 -0400

    feat: add primeiro component com client side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,14 @@ import { ArrowFunction, PrimeiroComponent } from "../components/PrimeiroComponen
 export default function Home() {
   return (
     <div>
-      <PrimeiroComponent />
+      <PrimeiroComponent 
+        mensagem="Olá, esta mensagem estou passando como parâmetro." 
+        mensagemDoBotao="Esta é a mensagem do botão"/>
+
+      <PrimeiroComponent 
+        mensagem="Sou outro componente." 
+        mensagemDoBotao="Clicaram em mim."/>
+
       <ArrowFunction />
     </div>
 
--- src/components/PrimeiroComponent.tsx
+++ src/components/PrimeiroComponent.tsx
@@ -1,16 +1,27 @@
 'use client'
 
-export function PrimeiroComponent(){
-    function handleClick(){
-        console.log("Botão clicado!");
+interface PrimeiroComponentProps{
+    //A interrogação diz que a propriedade é opcional, ou seja, o componente pode ser renderizado sem passar um valor para essas propriedades.
+    mensagem?:string;
+    mensagemDoBotao?:string;
+}
+
+//export function PrimeiroComponent(){} //Até Aula 29
+  
+//converti em arroyfunction utilizando o props
+export const PrimeiroComponent:React.FC<PrimeiroComponentProps> = (props:PrimeiroComponentProps) =>{
+    function handleClick(){        
+        console.log(props.mensagemDoBotao );
     }
+
     return (
         <div>
-            Meu primeiro componente!
+            {props.mensagem}
             <button onClick={handleClick} >Clique aqui</button>
         </div>
     )
 }
+
 
 export const ArrowFunction = ()=>{
     return (
