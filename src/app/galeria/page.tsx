72b8c6a97e311b99ef8b402ac411f62f14caf7ac
ricardosanto-crypto <ricardo.santo@gmail.com>
2026-04-24 23:06:44 -0400

    39. Criando o formulário para pesquisa de imagens

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -73,8 +73,17 @@ export default function GaleriaPage(){
     }    
 
     return (
-       <Template>     
-            <button className='bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded' onClick={searchImages}>Clique para mudar</button>                 
+       <Template>                 
+            <section className="flex flex-col items-center justify-center my-5">
+                <div className="fles space-x-4">
+                    <input type="text" className="border px-3 py-2 rounded-1g text-gray-900"/>
+                    <select className="border px-4 py-2 rounded-lg text-gray-900">
+                        <option>Todos</option>
+                    </select>
+                    <button className="bg-blue-500 text-white py-2 px-4 rounded-1g" onClick={searchImages}>Search</button>                
+                    <button className="bg-yellow-500 text-white py-2 px-4 rounded-1g">Add New</button>                
+                </div>                
+            </section>
             <section className="grid grid-cols-3 gap-8">                                                  
               {
                 renderImageCards()
