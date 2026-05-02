7a06e20effe7f0701625a1462bb2a9905e68e0e0
ricardosanto-crypto <ricardo.santo@gmail.com>
2026-05-02 17:00:54 -0400

    Aula 40. Implementação da lógica de pesquisa de imagens

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-import{ Template, ImageCard } from "@/components";
-import {Image} from '@/resources/image/image.resource';
-import {useImageService} from '@/resources/image/image.service';
-import{useState} from 'react';
+import { Template, ImageCard } from "@/components";
+import { Image } from '@/resources/image/image.resource';
+import { useImageService } from '@/resources/image/image.service';
+import { useState } from 'react';
 
-export default function GaleriaPage(){
+export default function GaleriaPage() {
 
     /*const image1 = "https://images.unsplash.com/photo-1768489002497-12453d8cfe5a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";    
     const image2 = "https://plus.unsplash.com/premium_photo-1701764128909-68204034c453?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
@@ -50,46 +50,60 @@ export default function GaleriaPage(){
 
     const useService = useImageService();
     const [images, setImages] = useState<Image[]>([])
-    
-    async function searchImages(){
-        const result = await useService.buscar();
+    const [query, setQuery] = useState<string>('');
+    const [extension, setExtension] = useState<string>('');
+
+    async function searchImages() {
+        console.log("valor digitado: ", query);
+        console.log("extensão digitada: ", extension);
+        const result = await useService.buscar(query, extension);
         setImages(result);
-        console.table(result);
+        //console.table(result);
     }
-        
-    function renderImageCard(image:Image){
+
+    function renderImageCard(image: Image) {
         return (
-            <ImageCard  nome={image.name} 
-                        src={image.url} 
-                        tamanho={image.size} 
-                        dataUpload={image.uploadDate}/> 
+            <ImageCard
+                key={image.url}
+                nome={image.name}
+                src={image.url}
+                tamanho={image.size}
+                dataUpload={image.uploadDate} />
             //<ImageCard nome="nome_iamgem" tamanho="10MB" dataUpload='24/04/2026' src="https://images.unsplash.com/photo-1768489002497-12453d8cfe5a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className='h-56 w-full object-cover rounded-t-md'/>                           
         )
     }
 
-    function renderImageCards(){
-        //return images.map(image => image.name);
+    function renderImageCards() {
+        //return images.map(image => image.name); neste caso o map retorna apenas o nome da imagem.
+        //return images.map(image => renderImageCard(image));
         return images.map(renderImageCard);
-    }    
+    }
 
     return (
-       <Template>                 
+        <Template>
             <section className="flex flex-col items-center justify-center my-5">
                 <div className="fles space-x-4">
-                    <input type="text" className="border px-3 py-2 rounded-1g text-gray-900"/>
-                    <select className="border px-4 py-2 rounded-lg text-gray-900">
-                        <option>Todos</option>
+                    <input type="text"
+                        onChange={event => setQuery(event.target.value)}
+                        className="border px-3 py-2 rounded-1g text-gray-900" />
+                    <select className="border px-4 py-2 rounded-lg text-gray-900" onChange={event => setExtension(event.target.value)}>
+                        <option value="">Todos</option>
+                        <option value="jpg">JPG</option>
+                        <option value="jpeg">JPEG</option>
+                        <option value="png">PNG</option>
+                        <option value="gif">GIF</option>
+                        <option value="webp">WEBP</option>
                     </select>
-                    <button className="bg-blue-500 text-white py-2 px-4 rounded-1g" onClick={searchImages}>Search</button>                
-                    <button className="bg-yellow-500 text-white py-2 px-4 rounded-1g">Add New</button>                
-                </div>                
+                    <button className="bg-blue-500 text-white py-2 px-4 rounded-1g" onClick={searchImages}>Search</button>
+                    <button className="bg-yellow-500 text-white py-2 px-4 rounded-1g">Add New</button>
+                </div>
             </section>
-            <section className="grid grid-cols-3 gap-8">                                                  
-              {
-                renderImageCards()
-              }
-            </section>  
-       </Template>
+            <section className="grid grid-cols-3 gap-8">
+                {
+                    renderImageCards()
+                }
+            </section>
+        </Template>
     )
 
 }
--- src/resources/image/image.service.ts
+++ src/resources/image/image.service.ts
@@ -1,11 +1,13 @@
 //obs: se a classe Image fosse default, a importação seria feita sem as chaves, ou seja, import Image from './image.resource'   
-import {Image} from './image.resource'
+import { Image } from './image.resource'
 
 class ImageService {
     baseURL: string = 'http://localhost:8080/v1/images';
 
-    async buscar() : Promise<Image[]> {
-        const response = await fetch(this.baseURL);        
+    async buscar(query: string = "", extension: string = ""): Promise<Image[]> {
+        const url = `${this.baseURL}?query=${query}&extension=${extension}`;
+        console.log("URL de busca: ", url);
+        const response = await fetch(url);
         return await response.json();
     }
 }
