07a12aecf4b315288776a9aa08e13adb91a36ea1
ricardosanto-crypto <ricardo.santo@gmail.com>
2026-09-25 17:49:56 -0400

    Aula 46. Configurando a navegação entre as páginas - adicionando link

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -4,6 +4,7 @@ import { Template, ImageCard } from "@/components";
 import { Image } from '@/resources/image/image.resource';
 import { useImageService } from '@/resources/image/image.service';
 import { useState } from 'react';
+import Link from "next/link";
 
 export default function GaleriaPage() {
 
@@ -99,7 +100,9 @@ export default function GaleriaPage() {
                         <option value="gif">GIF</option>
                     </select>
                     <button className="bg-blue-500 text-white py-2 px-4 rounded-1g rounded-lg hover:bg-blue-300" onClick={searchImages}>Search</button>
-                    <button className="bg-yellow-500 text-white py-2 px-4 rounded-1g rounded-lg hover:bg-yellow-300">Add New</button>
+                    <Link href="/formulario">
+                        <button className="bg-yellow-500 text-white py-2 px-4 rounded-1g rounded-lg hover:bg-yellow-300">Add New</button>
+                    </Link>
                 </div>
             </section>
             <section className="grid grid-cols-3 gap-8">
