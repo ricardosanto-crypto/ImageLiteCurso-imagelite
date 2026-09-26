bc15a9a48904e62cf88dc8d28d93a54cc64022a1
ricardosanto-crypto <ricardo.santo@gmail.com>
2026-09-26 09:43:34 -0400

    Aula 50. Finalizando a casca do formulário

--- src/app/formulario/page.tsx
+++ src/app/formulario/page.tsx
@@ -1,26 +1,29 @@
-import { InputText, Template } from "@/components";
+import { Button, InputText, Template } from "@/components";
+import Link from "next/link";
 
-export default function FormularioPage(){
+export default function FormularioPage() {
     return (
         <Template>
             <section className="flex flex-col items-center justify-center my-5">
                 <h5 className="text-2xl font-bold mb-4">Nova Imagem</h5>
                 <form>
-                    <div className="mt-5 grid grid-cols-1"> 
+                    <div className="mt-5 grid grid-cols-1">
                         <label className="block text-sm font-medium leading-6 text-gray-700">Name: *</label>
                         <InputText placeholder="type the image's name" />
                     </div>
-                    <div className="mt-5 grid grid-cols-1"> 
+                    <div className="mt-5 grid grid-cols-1">
                         <label className="block text-sm font-medium leading-6 text-gray-700">Tags: *</label>
                         <InputText placeholder="type the tags comma separated" />
-                    </div>  
-                    <div className="mt-5 grid grid-cols-1"> 
+                    </div>
+                    <div className="mt-5 grid grid-cols-1">
                         <label className="block text-sm font-medium leading-6 text-gray-700">Image: *</label>
                         <div className="mt-2 flex justify-center rounded-lg border border-dashed-gray-900/25 px-6 py-10">
                             <div className='text-center'>
                                 <svg className="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" />
-                                </svg>        
+                                    <path fillRule="evenodd" 
+                                        d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" 
+                                        clipRule="evenodd" />
+                                </svg>
                                 <div className="mt-4 flex text-sm leading-6 text-gray-600">
                                     <label htmlFor="file-upload" className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
                                         <span>Upload a file</span>
@@ -28,12 +31,18 @@ export default function FormularioPage(){
                                     </label>
                                     <p className="pl-1">or drag and drop</p>
                                 </div>
-                                <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>             
-                            </div>                            
+                                <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                            </div>
                         </div>
-                    </div>  
+                    </div>
+                    <div className="mt-5 flex items-center justify-end gap-x-4">
+                        <Button style="bg-blue-500 hover:bg-blue-300" type="submit" label="Save" />
+                        <Link href="/galeria">
+                            <Button style="bg-red-500 hover:bg-red-300" label="Cancel" />                        
+                        </Link>
+                    </div>
                 </form>
             </section>
-       </Template>
+        </Template>
     )
 }
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -4,11 +4,13 @@ interface ButtonProps {
     style?: string
     label?: string;
     onClick?: (event: any) => void;
+    type?: "button" | "submit" | "reset" | undefined;
 }
-export const Button: React.FC<ButtonProps> = ({ style, label, onClick }: ButtonProps) => {
+export const Button: React.FC<ButtonProps> = ({ style, label, onClick, type }: ButtonProps) => {
     return (
         <button className={`${style} text-white py-2 px-4 rounded-lg`} 
-            onClick={onClick}>
+            onClick={onClick}
+            type={type}>
             {label}
         </button>
     )
